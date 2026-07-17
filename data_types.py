9ca0b12800e27f638efa5001a1c198935f609395
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-17 23:13:32 +0200

    8 main datatypes

---
+++ data_types.py
@@ -0,0 +1,53 @@
+# Python Data Types Example
+
+# Integer
+age = 27
+
+# Float
+salary = 50000.75
+
+# String
+name = "Yash"
+
+# Boolean
+is_active = True
+
+# List
+skills = ["Java", "Python", "Linux"]
+
+# Tuple
+coordinates = (10, 20)
+
+# Dictionary
+employee = {
+    "name": "Yash",
+    "role": "Backend Developer",
+    "experience": 3
+}
+
+# Set
+unique_numbers = {1, 2, 3, 3}
+
+
+print(age);
+print(type(age));
+
+print(salary)
+print(type(salary));
+
+print(name);
+print(type(name));
+
+print(is_active);
+print(type(is_active));
+print(skills);
+print(type(skills));
+
+print(coordinates);
+print(type(coordinates));
+
+print(employee);
+print(type(employee));
+
+print(unique_numbers);
+print(type(unique_numbers));
