aeec58b4a002453b7773cf2dd9fcec59fd86d285
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-17 23:21:17 +0200

    ef elif else simple code

---
+++ if_elif_else.py
@@ -0,0 +1,18 @@
+# Real life example: Employee performance rating
+
+rating = int(input("Enter performance rating (1-5): "))
+
+if rating == 5:
+    print("Excellent Performance")
+
+elif rating == 4:
+    print("Very Good Performance")
+
+elif rating == 3:
+    print("Good Performance")
+
+elif rating == 2:
+    print("Need Improvement")
+
+else:
+    print("Poor Performance")
