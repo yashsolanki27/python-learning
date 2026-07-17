933f5cdd66c25873db0f7a17d29218e983552d20
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-17 23:17:05 +0200

    easy input

---
+++ input_output.py
@@ -0,0 +1,10 @@
+# Simple employee information program
+
+employee_name = input("Enter employee name: ")
+employee_id = input("Enter employee ID: ")
+salary = float(input("Enter salary: "))
+
+print("\nEmployee Details")
+print("Name:", employee_name)
+print("ID:", employee_id)
+print("Salary:", salary)
