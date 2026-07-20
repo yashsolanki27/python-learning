0212a320e92a494d3ce208324e9f224435dd844b
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-20 23:45:47 +0200

    string handling

---
+++ string.py
@@ -0,0 +1,16 @@
+name = "Yash"
+
+print(name)
+print(name.upper())
+print(name.lower())
+print(len(name))
+
+
+#more adv
+email = "  yash@gmail.com  "
+
+print(email.strip())          # Remove spaces
+print(email.replace("gmail", "outlook"))
+print(email.startswith("yash"))
+print(email.endswith(".com"))
+print(email.split("@"))
