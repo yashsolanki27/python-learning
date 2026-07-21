d4a700668266f5bbd6648baa888eb5831ba4054c
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-21 16:34:06 +0200

    importing tools

---
+++ impo.py
@@ -0,0 +1,19 @@
+# import pandas as pd
+
+# !pip install pandas
+# # # Pattern 1: Import the whole module
+# # import math
+# # # Now use: math.sqrt(16)
+
+# # # Pattern 2: Import specific items from a module
+# # from math import sqrt, pi
+# # # Now use: sqrt(16)
+
+
+# # # import random
+
+
+# # # import os 
+# # # current_dir = os.getcwd()
+# # # print(current_dir)
+# #  
