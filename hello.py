a4f3767d2817150adabbe10ad29a5c82b063deff
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-21 16:33:34 +0200

    speed run the basics

---
+++ hello.py
@@ -0,0 +1,56 @@
+# # # %%
+# # first_name = "Yash"
+# # last_name = "Solanki"
+
+
+# # full_name = first_name + " " + last_name
+# # print(full_name)
+
+# # long_dash = "_" * 12
+
+# # print(full_name)
+# # print(long_dash)
+
+# # len(full_name)
+# # len(long_dash)
+
+# # # %%
+# # %%
+# # is_logged_in = True;
+# # %%
+
+
+# # is_true = True;
+
+# # age = 18;
+
+# # can_vote = age >=18  
+# # %%
+
+# temp = 2031
+
+
+# if temp > 30:
+#   print("it's very hot")
+# elif temp > 25 : 
+#  print("hot")
+# else : 
+#  print("Nice weather")
+# # %%
+# # 
+
+# score = 85
+
+# if score >= 90:
+#     print("A - Excellent!")
+# elif score >= 80:
+#     print("B - Good job!")
+# elif score >= 70:
+#     print("C - Keep it up!")
+# else:
+#     print("F - Need improvement")
+
+
+
+for i in range[5]:
+     print(i)
