dd4a4dbc6846d9687cb444198a3c0a1f7bd9ed4e
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-21 16:31:08 +0200

    working with dictionary

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+##dictionaries
+
+# Empty dictionary
+my_dict = {}
+
+# Dictionary with data
+person = {
+    "name": "Yash",
+    "age": 27,
+    "city": "Voorburg"
+}
+
+person["name"] = "Yash";
+person["licence"] = True;
+
+person["game"] = True;
+
+print(person)
+
+del person['Voorburg']
+
+
+
+
+
+# # Different ways to create
+# scores = dict(math=95, english=87, science=92)
