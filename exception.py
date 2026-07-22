7fa8b89678f525e01a0fc8a1363f2ac3af372260
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-22 16:55:38 +0200

    Exception handling

--- exception.py
+++ exception.py
@@ -11,12 +11,11 @@ except ValueError:
 
     #advance 
 
-    try:
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-
-    result = num1 / num2
-    print(result)
+try:
+       num1 = int(input("Enter first number: "))
+       num2 = int(input("Enter second number: "))
+       result = num1 / num2
+       print(result)
 
 except ValueError:
     print("Invalid input")
