18969c9eee7fd9db0c174c5a40e4b071c2eab716
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-26 20:42:54 +0200

    Lists Syntax

---
+++ lists-drills/listssyntax.py
@@ -0,0 +1,21 @@
+# my_list[0]
+
+# my_list[-1]
+
+# my_list[1:4]
+
+# my_list.append(item)
+
+# my_list.insert(index, item)
+
+# my_list.remove(item)
+
+# my_list.pop(index)
+
+# my_list.sort()
+
+# my_list.reverse()
+
+# nested[0][1]
+
+# [value for value in list]
