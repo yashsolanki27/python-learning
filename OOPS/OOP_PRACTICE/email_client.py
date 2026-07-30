842954726852fe18b3d0f97ecaa0666598adc14b
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-30 21:59:38 +0200

    Email-client-class

---
+++ OOPS/OOP_PRACTICE/email_client.py
@@ -0,0 +1,16 @@
+class EmailCleint:
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def send_email(self, email):
+        self.email = email
+        print(f"Using API KEY: {self.api_key}")
+        print(f"To: {self.email}")
+        print("Subject : Hello")
+        print("Body: How are you?")
+
+
+email1 = EmailCleint("asfnjhwtg76nksdhf3bkhjcsdnaUY34G254MGHABDTBU3Y327NUHknJb")
+
+email1.send_email("JAVSADASD123@gmail.com")
