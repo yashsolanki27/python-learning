023bc8ce5e84bff98f8ebd359f844fb8261566b6
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-30 21:40:12 +0200

    class-method2-finish

--- OOPS/OOP_PRACTICE/first_class.py
+++ OOPS/OOP_PRACTICE/first_class.py
@@ -3,7 +3,10 @@ class Person:
     def __init__(self, name):
         self.name = name
 
+    def greet(self):
+        print(f"Hello:{self.name}")
 
-person1 = Person("YASH")
 
-print(person1.name)
+person1 = Person("YASH SOLANKI")
+
+person1.greet()
