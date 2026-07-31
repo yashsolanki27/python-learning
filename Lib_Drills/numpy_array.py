d77ef2d56f7d8b854d9700194cef35dc9aa892d0
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-31 22:11:30 +0200

    indexing-slicing-numpy

--- Lib_Drills/numpy_array.py
+++ Lib_Drills/numpy_array.py
@@ -24,3 +24,20 @@ print(student_age)
 arr = np.array([5, 10, 15])
 
 print(arr + 1000)
+
+arr * 2  # multiply same value with every element
+
+
+# Indexing
+# Index = position number of a array
+
+tea = [10, 20, 30]
+# in python
+print(tea[1])
+
+
+# Slicing -- extracting a portion of data
+
+arr2 = np.array([10, 20, 30, 40, 50, 60, 400])
+# means start at index 0 and stop before index 4
+arr2[1:4]
