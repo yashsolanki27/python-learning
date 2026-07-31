f04193fe9af156c1a0b1c3e92639333c799b3662
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-31 22:03:40 +0200

    vectorized-operations-numpy

--- Lib_Drills/numpy_array.py
+++ Lib_Drills/numpy_array.py
@@ -1,5 +1,26 @@
 import numpy as np
 
-arrs = np.array([10, 20, 30, 40, 50, 60])
+# NumPy Arrays  → Fast numerical operations
+# creating an array
+arrs = np.array(
+    [10, 20, 30, 40, 50, 60]
+)  # np.array is a function, so you must call it using ()
 
 print(arrs)
+
+
+student_marks = [80, 55, 46, 24, 78]  # python list
+
+print(student_marks)
+
+student_age = np.array([27, 28, 18, 19, 22])  # numpy array
+# why?????????????? bcos numpy can perform calc on all elements at once
+
+print(student_age)
+
+
+# Vectorized Operations  --> perform the same operation on every element without writing loops
+
+arr = np.array([5, 10, 15])
+
+print(arr + 1000)
