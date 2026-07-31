cb136aff7fa6eb635a5a8d4eb490655a7d60cf8e
Yashsolanki27 <solankiyash7227@gmail.com>
2026-07-31 23:22:43 +0200

    final-pandas-code

--- Lib_Drills/pandas_drills.py
+++ Lib_Drills/pandas_drills.py
@@ -42,3 +42,30 @@ emp_df = pd.DataFrame(data)
 
 print(emp_df.groupby("Department")["Salary"].mean())
 print(emp_df.groupby("Department")["Salary"].sum())
+
+
+##final program to cover all stuff
+
+
+# Read CSV
+df = pd.read_csv("employees.csv")
+
+# Preview first 5 rows
+print("\nHEAD")
+print(df.head())
+
+# Rows and columns
+print("\nSHAPE")
+print(df.shape)
+
+# Select one column
+print("\nSALARY COLUMN")
+print(df["Salary"])
+
+# Filter
+print("\nSALARY > 50000")
+print(df[df["Salary"] > 50000])
+
+# Group By Average
+print("\nAVERAGE SALARY BY DEPARTMENT")
+print(df.groupby("Department")["Salary"].mean())
